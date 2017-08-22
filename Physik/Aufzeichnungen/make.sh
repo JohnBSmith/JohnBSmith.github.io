e65a7f78e80678b531df45a6b905edaa7a75e5e7
finn <none>
2017-08-22 14:11:49 +0200

    update

---
+++ Physik/Aufzeichnungen/make.sh
@@ -0,0 +1,6 @@
+
+mkdir -p /tmp/Physik-Aufzeichnungen
+pdflatex -output-directory /tmp/Physik-Aufzeichnungen Physik-Aufzeichnungen.tex 
+mv /tmp/Physik-Aufzeichnungen/Physik-Aufzeichnungen.pdf ./
+
+
