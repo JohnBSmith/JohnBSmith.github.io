8e7a11823b6ab3a1955fda3cbf68bb92c223c821
finn <finn@c1>
2018-12-12 17:24:57 +0100

    update

---
+++ prog/clear.py
@@ -0,0 +1,52 @@
+
+# This script removes all files that are generated
+# from source code. Please run build.py, to generate
+# PDF files from LaTeX source code automatically.
+
+# Dieses Skript entfernt alle Dateien die aus Quelltext
+# erzeugt wurden. Bitte führe build.py aus um automatisch
+# PDF-Dateien aus LaTeX-Quelltext zu erzeugen.
+
+files = [
+"./Mathematik/Grundlagen/Metatheoreme/Metatheoreme.pdf",
+"./Mathematik/Grundlagen/Peano-Axiome/Peano-Axiome.pdf",
+"./Mathematik/Grundlagen/Grundlagen.pdf",
+"./Mathematik/Skript-Mathematik/Skript-Mathematik.pdf",
+"./Mathematik/Formelzettel/Formelzettel.pdf",
+"./Mathematik/Gauss/Gauss.pdf",
+"./Mathematik/FM/FM.pdf",
+"./Mathematik/Aufzeichnungen/Aufzeichnungen.pdf",
+"./Mathematik/Dgln/LGDG/LGDG.pdf",
+"./Mathematik/Dgln/Dgln.pdf",
+"./Mathematik/Rechnen/Rechnen.pdf",
+"./Mathematik/Kombinatorik/Kombinatorik.pdf",
+"./Mathematik/Algebra/Algebra.pdf",
+"./Mathematik/Konventionen/Konventionen.pdf",
+"./Mathematik/WK/WK.pdf",
+"./Mathematik/Geometrie/Geraden/Geraden.pdf",
+"./Mathematik/Geometrie/Ableitung/Ableitung.pdf",
+"./Mathematik/Analysis/Fourier-Analysis/Fourier-Analysis.pdf",
+"./Mathematik/Analysis/Ableitung/Ableitung.pdf",
+"./Mathematik/Beweisarchiv/Beweisarchiv.pdf",
+"./Mathematik/EWP/EWP.pdf",
+"./Mathematik/Aufgaben/Aufgaben.pdf",
+"./Mathematik/KD/KD.pdf",
+"./Physik/Aufzeichnungen/Physik-Aufzeichnungen.pdf",
+"./Physik/ET/ET.pdf",
+"./Physik/ED/ED.pdf",
+"./Physik/Mechanikbuch/Mechanik.pdf",
+"./Mathematics/HM/HM.pdf",
+"./Mathematics/sf-implementation/sf-implementation.pdf",
+"./Chemie/PC/PC.pdf",
+"./Chemie/PCH/PCH.pdf",
+"./prog/index.js"
+]
+
+import os
+
+os.chdir("..")
+for path in files:
+    if os.path.exists(path):
+        os.remove(path)
+
+
--- prog/makeindex.py
+++ prog/makeindex.py
@@ -15,104 +15,104 @@ charmap = {
 }
 
 def key_function(t):
-  return "".join((charmap[c] if c in charmap else c) for c in t[0].lower())
+    return "".join((charmap[c] if c in charmap else c) for c in t[0].lower())
 
 def sift(s,n):
-  return set(text for text in regex_alpha.findall(s) if len(text)>n)
+    return set(text for text in regex_alpha.findall(s) if len(text)>n)
 
 def get_indexlist():
-  f = open("indexlist.txt","r")
-  m = sift(f.read(),1)
-  f.close()
-  return m
+    f = open("indexlist.txt","r")
+    m = sift(f.read(),1)
+    f.close()
+    return m
 
 def index_append(d,text,name,path):
-  if text in d:
-    d[text].append([name,path])
-  else:
-    d[text]=[[name,path]]
+    if text in d:
+        d[text].append([name,path])
+    else:
+        d[text] = [[name,path]]
 
 def is_indexable(s):
-  return s[-4:]==".htm" or s[-4:]==".txt" or s[-4:]==".tex"
+    return s[-4:]==".htm" or s[-4:]==".txt" or s[-4:]==".tex"
 
 def generate_index():
-  a = list(os.walk("./"))
-  d={}
-  for t in a:
-    for name in t[2]:
-      if is_indexable(name):
-        path = os.path.join(t[0],name)
-        f = open(path,'r')
-        s = f.read()
-        f.close()
-        for text in sift(s,2):
-          index_append(d,text,name,path)
-  for text,L in d.items():
-    L.sort(key=lambda t: t[0].lower())
-  index = list(d.items())
-  index.sort(key=key_function)
-  return index
+    a = list(os.walk("./"))
+    d = {}
+    for t in a:
+        for name in t[2]:
+            if is_indexable(name):
+                path = os.path.join(t[0],name)
+                f = open(path,'r')
+                s = f.read()
+                f.close()
+                for text in sift(s,2):
+                    index_append(d,text,name,path)
+    for text, L in d.items():
+        L.sort(key=lambda t: t[0].lower())
+    index = list(d.items())
+    index.sort(key=key_function)
+    return index
 
 def generate_index_exclusive(index_set):
-  a = list(os.walk("./"))
-  d={}
-  for t in a:
-    for name in t[2]:
-      if is_indexable(name):
-        path = os.path.join(t[0],name)
-        f = open(path,'r')
-        s = f.read()
-        f.close()
-        for text in index_set:
-          if text in s:
-            index_append(d,text,name,path)
-  for text,L in d.items():
-    L.sort(key=lambda t: t[0].lower())
-  index = list(d.items())
-  index.sort(key=key_function)
-  return index
+    a = list(os.walk("./"))
+    d = {}
+    for t in a:
+        for name in t[2]:
+            if is_indexable(name):
+                path = os.path.join(t[0],name)
+                f = open(path,'r')
+                s = f.read()
+                f.close()
+                for text in index_set:
+                    if text in s:
+                        index_append(d,text,name,path)
+    for text, L in d.items():
+        L.sort(key=lambda t: t[0].lower())
+    index = list(d.items())
+    index.sort(key=key_function)
+    return index
 
 def file_list_to_html(a):
-  buffer=[]
-  for name,path in a:
-    buffer.append("<a href=\""+path+"\">"+name+"</a>")
-  return ", ".join(buffer)
+    buffer = []
+    for name, path in a:
+        buffer.append("<a href=\""+path+"\">"+name+"</a>")
+    return ", ".join(buffer)
 
 def index_to_html(index):
-  buffer=[]
-  for text,a in index:
-    buffer.append("\n<dt>"+text)
-    buffer.append("\n<dd>"+file_list_to_html(a))
-  return "".join(buffer)
+    buffer = []
+    for text, a in index:
+        buffer.append("\n<dt>"+text)
+        buffer.append("\n<dd>"+file_list_to_html(a))
+    return "".join(buffer)
 
 def wlist(index):
   return "\n".join(text for text,a in index)
 
 def file_list_to_json(a):
-  buffer=[]
-  for name,path in a:
-    buffer.append('"{}"'.format(path))
-  return "["+", ".join(buffer)+"]"
+    buffer = []
+    for name, path in a:
+        buffer.append('"{}"'.format(path))
+    return "["+", ".join(buffer)+"]"
 
 def index_to_json(index):
-  buffer=[]
-  for text,a in index:
-    buffer.append("[\""+text+"\","+file_list_to_json(a)+"]")
-  return "\n,".join(buffer)
+    buffer = []
+    for text, a in index:
+        buffer.append("[\""+text+"\","+file_list_to_json(a)+"]")
+    return "\n,".join(buffer)
 
 def main():
-  index_set = get_indexlist()
-  cwd = os.getcwd()
-  os.chdir(sys.argv[1])
-  index = generate_index()
-  os.chdir(cwd)
-  s = "index=["+index_to_json(index)+"\n];\n"
-  f = open("index.js", "w")
-  f.write(s)
-  f.close()
-  f = open("wlist.txt", "w")
-  f.write(wlist(index))
-  f.close()
+    index_set = get_indexlist()
+    cwd = os.getcwd()
+    os.chdir(sys.argv[1])
+    index = generate_index()
+    os.chdir(cwd)
+    s = "index=["+index_to_json(index)+"\n];\n"
+    f = open("index.js", "w")
+    f.write(s)
+    f.close()
+    f = open("wlist.txt", "w")
+    f.write(wlist(index))
+    f.close()
 
 main()
 
