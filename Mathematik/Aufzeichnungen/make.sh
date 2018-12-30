23733f133d741295d94a700f0ecb203f6a7ce96a
finn <finn@c1>
2018-12-30 12:52:45 +0100

    update

--- Mathematik/Aufzeichnungen/make.sh
+++ Mathematik/Aufzeichnungen/make.sh
@@ -1,6 +1,6 @@
 
 name=Aufzeichnungen
-path=/tmp/$name-d3ca
+path=/tmp/$name-d35b
 
 mkdir -p $path
 pdflatex -output-directory $path $name.tex
---
+++ prog/build.py
@@ -0,0 +1,57 @@
+
+# This script generates PDF files from LaTeX source code.
+# Each listed path refers to a document to be generated.
+
+# Dieses Skript erzeugt PDF-Dateien aus LaTeX-Quelltext.
+# Jeder aufgelistete Pfad führt zu einem Dokument, das erzeugt
+# werden soll.
+
+paths = [
+"./Mathematik/Grundlagen/Metatheoreme/",
+"./Mathematik/Grundlagen/Peano-Axiome/",
+"./Mathematik/Grundlagen/",
+"./Mathematik/Skript-Mathematik/",
+"./Mathematik/Formelzettel/",
+"./Mathematik/Gauss/",
+"./Mathematik/FM/",
+"./Mathematik/Aufzeichnungen/",
+"./Mathematik/Dgln/LGDG/",
+"./Mathematik/Dgln/",
+"./Mathematik/Rechnen/",
+"./Mathematik/Kombinatorik/",
+"./Mathematik/Algebra/",
+"./Mathematik/Konventionen/",
+"./Mathematik/WK/",
+"./Mathematik/Geometrie/Geraden/",
+"./Mathematik/Geometrie/Ableitung/",
+"./Mathematik/Analysis/Fourier-Analysis/",
+"./Mathematik/Analysis/Ableitung/",
+"./Mathematik/Beweisarchiv/",
+"./Mathematik/EWP/",
+"./Mathematik/Aufgaben/",
+"./Mathematik/KD/",
+"./Physik/Aufzeichnungen/",
+"./Physik/ET/",
+"./Physik/ED/",
+"./Physik/Mechanikbuch/",
+"./Mathematics/HM/",
+"./Mathematics/sf-implementation/",
+"./Chemie/PC/",
+"./Chemie/PCH/",
+]
+
+import os
+
+os.chdir("..")
+wd = os.getcwd()
+
+def make(path):
+    for i in range(2):
+        print("\n#\n#\n# "+path+"\n#\n#\n#\n\n")
+        os.system("sh make.sh")
+
+for path in paths:
+    os.chdir(wd)
+    os.chdir(path)
+    make(path)
+
