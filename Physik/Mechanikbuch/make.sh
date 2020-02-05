a9dce304d3edf697ff40ff330427cd8579b82df2
finn <finn@c1>
2020-02-06 00:56:27 +0100

    update

--- Physik/Mechanikbuch/make.sh
+++ Physik/Mechanikbuch/make.sh
@@ -3,5 +3,7 @@ name=Mechanik
 path=/tmp/$name-c824
 
 mkdir -p $path
-pdflatex -output-directory $path $name.tex
-mv $path/$name.pdf ./
+pdflatex -output-format dvi -output-directory $path $name.tex
+dvipdfmx $path/$name.dvi
+# mv $path/$name.pdf ./
+
