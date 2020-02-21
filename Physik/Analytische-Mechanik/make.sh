dbfc096c8e52a507c0aeafb2df118abac89e45e1
finn <finn@c1>
2020-02-21 06:23:14 +0100

    update

---
+++ Physik/Analytische-Mechanik/make.sh
@@ -0,0 +1,9 @@
+
+name=Analytische-Mechanik
+path=/tmp/$name-af57
+
+mkdir -p $path
+pdflatex -output-format dvi -output-directory $path $name.tex
+dvipdfmx $path/$name.dvi
+# mv $path/$name.pdf ./
+
