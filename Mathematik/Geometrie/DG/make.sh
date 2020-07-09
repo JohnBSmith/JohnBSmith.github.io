0d2141a953e91282440aa937f58362d648a70272
finn <finn@c1>
2020-07-09 17:15:33 +0200

    update

--- Mathematik/Geometrie/DG/make.sh
+++ Mathematik/Geometrie/DG/make.sh
@@ -3,8 +3,13 @@ name=DG
 path=/tmp/$name-ab2d
 
 mkdir -p $path
-pdflatex -output-directory $path $name.tex
+
+# pdflatex -output-directory $path $name.tex
+# mv $path/$name.pdf ./
+pdflatex -output-format dvi -output-directory $path $name.tex
+dvipdfmx $path/$name.dvi
+
 (cd $path && makeindex $name.idx)
 mv $path/$name.ind ./
-mv $path/$name.pdf ./
+
 
--- Mathematik/Grundlagen/Band1-Grundlagen/make.sh
+++ Mathematik/Grundlagen/Band1-Grundlagen/make.sh
@@ -3,8 +3,13 @@ name=Grundlagen
 path=/tmp/$name-bf40
 
 mkdir -p $path
-pdflatex -output-directory $path $name.tex
+
+#pdflatex -output-directory $path $name.tex
+#mv $path/$name.pdf ./
+pdflatex -output-format dvi -output-directory $path $name.tex
+dvipdfmx $path/$name.dvi
+
 (cd $path && makeindex $name.idx)
 mv $path/$name.ind ./
-mv $path/$name.pdf ./
+
 
