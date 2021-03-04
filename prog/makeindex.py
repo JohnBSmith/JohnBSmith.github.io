3948df05e9c5909e5115834b979fe46de20e4cf9
finn <finn@c1>
2021-03-04 19:47:27 +0100

    update

--- prog/makeindex.py
+++ prog/makeindex.py
@@ -9,9 +9,9 @@ charmap = {
   'ö': 'o',
   'ü': 'u',
   'ß': 'ss',
-  'A': 'A',
-  'Ö': 'Ö',
-  'Ü': 'Ü'
+  'Ä': 'A',
+  'Ö': 'O',
+  'Ü': 'U'
 }
 
 def key_function(t):
@@ -26,18 +26,48 @@ def get_indexlist():
     f.close()
     return m
 
-def index_append(d,text,name,path):
-    if text in d:
-        d[text].append([name,path])
-    else:
-        d[text] = [[name,path]]
+class Index:
+    def __init__(self):
+        self.pool = {}
+        self.index = {}
+
+    def append_path(self, path):
+        if path in self.pool:
+            self.pool[path]["count"] += 1
+        else:
+            self.pool[path] = {"count": 0}
+
+    def append(self,text,name,path):
+        self.append_path(path)
+        if text in self.index:
+            self.index[text].append([name,path])
+        else:
+            self.index[text] = [[name,path]]
+
+    def calculate_pool(self):
+        a = list(self.pool.items())
+
+        # The more often a pool index occurs,
+        # the shorter it should be.
+        a.sort(key = lambda t: -t[1]["count"])
+
+        for index,t in enumerate(a):
+            t[1]["index"] = index
+        self.pool_data = [t[0] for t in a]
+        
+    def finalize(self):
+        self.calculate_pool()
+        pool = self.pool
+        for record in self.index.items():
+            for t in record[1]:
+                t[1] = pool[t[1]]["index"]
 
 def is_indexable(s):
     return s[-4:]==".htm" or s[-4:]==".txt" or s[-4:]==".tex"
 
 def generate_index():
     a = list(os.walk("./"))
-    d = {}
+    index = Index()
     for t in a:
         for name in t[2]:
             if is_indexable(name):
@@ -46,12 +76,16 @@ def generate_index():
                 s = f.read()
                 f.close()
                 for text in sift(s,2):
-                    index_append(d,text,name,path)
-    for text, L in d.items():
+                    index.append(text,name,path)
+
+    index.finalize()
+    pool = index.pool_data
+
+    for text, L in index.index.items():
         L.sort(key=lambda t: t[0].lower())
-    index = list(d.items())
+    index = list(index.index.items())
     index.sort(key=key_function)
-    return index
+    return index, pool
 
 def generate_index_exclusive():
     index_set = get_indexlist()
@@ -92,23 +126,27 @@ def wlist(index):
 def file_list_to_json(a):
     buffer = []
     for name, path in a:
-        buffer.append('"{}"'.format(path))
-    return "["+", ".join(buffer)+"]"
+        buffer.append('{}'.format(path))
+    return "[" + ",".join(buffer) + "]"
 
 def index_to_json(index):
     buffer = []
     for text, a in index:
         buffer.append("[\""+text+"\","+file_list_to_json(a)+"]")
-    return "\n,".join(buffer)
+    return ",\n".join(buffer)
+
+def pool_to_json(pool):
+    return ",\n".join("\"" + path + "\"" for path in pool)
 
 def main():
     cwd = os.getcwd()
     os.chdir(sys.argv[1])
-    index = generate_index()
+    index, pool = generate_index()
     os.chdir(cwd)
-    s = "index=["+index_to_json(index)+"\n];\n"
+    buffer = ["pool=[\n", pool_to_json(pool), "\n];\nindex=[\n",
+        index_to_json(index), "\n];\n"]
     f = open("index.js", "w")
-    f.write(s)
+    f.write("".join(buffer))
     f.close()
     # f = open("wlist.txt", "w")
     # f.write(wlist(index))
--- prog/search.js
+++ prog/search.js
@@ -1,76 +1,77 @@
 
 function get_index(s){
-  for(var i=0; i<index.length; i++){
-    if(s==index[i][0].toLowerCase()) return i;
-  }
-  return -1;
+    for(var i=0; i<index.length; i++){
+        if(s==index[i][0].toLowerCase()) return i;
+    }
+    return -1;
 }
 
 function get_indizes(s){
-  var a=[];
-  for(var i=0; i<index.length; i++){
-    if(index[i][0].toLowerCase().indexOf(s)>=0){
-      a.push(i);
+    var a=[];
+    for(var i=0; i<index.length; i++){
+        if(index[i][0].toLowerCase().indexOf(s)>=0){
+            a.push(i);
+        }
     }
-  }
-  return a;
+    return a;
 }
 
 function last_part(s){
-  var i=s.length-1;
-  while(i>=0 && s[i]!='/') i--;
-  return s.substr(i+1);
+    var i=s.length-1;
+    while(i>=0 && s[i]!='/') i--;
+    return s.substr(i+1);
 }
 
 function get_ref(s){
-  return "../"+s.substr(2);
+    return "../"+s.substr(2);
 }
 
 function file_list_to_html(a){
-  var buffer=[];
-  for(var i=0; i<a.length; i++){
-    var x = "<a href='"+get_ref(a[i])+"'>"+last_part(a[i])+"</a>"
-    if(a[i].substr(a[i].length-4)==".tex"){
-      var s = a[i].slice(0,a[i].length-4);
-      x+=" (<a href='"+get_ref(s+".pdf")+"'>pdf</a>)";
+    var buffer=[];
+    for(var i=0; i<a.length; i++){
+        var path = pool[a[i]];
+        var x = "<a href='"+get_ref(path)+"'>"+last_part(path)+"</a>"
+        if(path.substr(path.length-4)==".tex"){
+            var s = path.slice(0,path.length-4);
+            x+=" (<a href='"+get_ref(s+".pdf")+"'>pdf</a>)";
+        }
+        buffer.push(x);
     }
-    buffer.push(x);
-  }
-  return buffer.join(", ");
+    return buffer.join(", ");
 }
 
 function index_to_html(i){
-  return "<dt>"+index[i][0]+"\n<dd>"+file_list_to_html(index[i][1]);
+    return "<dt>"+index[i][0]+"\n<dd>"+file_list_to_html(index[i][1]);
 }
 
 function search(){
-  var input = document.getElementById("input1");
-  var output = document.getElementById("output");
-  var s = input.value.toLowerCase();
-  if(s.length<2){
-    output.innerHTML = "Zeichenkette zu kurz";
-    return;
-  }
-  var i = get_index(s);
-  output.innerHTML = "<h3 class='info'>Exakt</h3>\n"
-  if(i<0){
-    output.innerHTML += "nichts gefunden<br><br>"
-  }else{
-    output.innerHTML += "<dl>"+index_to_html(i)+"</dl>";
-  }
-  var a = get_indizes(s);
-  if(a.length>0){
-    output.innerHTML += "<h3 class='info'>Als Teilzeichenkette</h3>";
-    var buffer=[];
-    for(i=0; i<a.length; i++){
-      buffer.push(index_to_html(a[i]));
+    var input = document.getElementById("input1");
+    var output = document.getElementById("output");
+    var s = input.value.toLowerCase();
+    if(s.length<2){
+        output.innerHTML = "Zeichenkette zu kurz";
+        return;
     }
-    output.innerHTML+="<dl>"+buffer.join("")+"</dl>";
-  }
+    var i = get_index(s);
+    output.innerHTML = "<h3 class='info'>Exakt</h3>\n"
+    if(i<0){
+        output.innerHTML += "nichts gefunden<br><br>"
+    }else{
+        output.innerHTML += "<dl>"+index_to_html(i)+"</dl>";
+    }
+    var a = get_indizes(s);
+    if(a.length>0){
+        output.innerHTML += "<h3 class='info'>Als Teilzeichenkette</h3>";
+        var buffer=[];
+        for(i=0; i<a.length; i++){
+            buffer.push(index_to_html(a[i]));
+        }
+        output.innerHTML+="<dl>"+buffer.join("")+"</dl>";
+    }
 }
 
 function handle_keys(event){
-  if(event.keyCode==13){
-    search();
-  }
+    if(event.keyCode==13){
+        search();
+    }
 }
