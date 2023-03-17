610ad651ecfbf1c74ceb92c69bcebb0502262b47
finn <finn.bcd@gmail.com>
2023-03-17 13:28:47 +0100

    update

---
+++ Mathematik/Grundlagen/GM/make.sh
@@ -0,0 +1,15 @@
+
+name=Grundlagen
+path=/tmp/$name-bf41
+
+mkdir -p $path
+
+#pdflatex -output-directory $path $name.tex
+#mv $path/$name.pdf ./
+pdflatex -output-format dvi -output-directory $path $name.tex
+dvipdfmx $path/$name.dvi
+
+(cd $path && makeindex $name.idx)
+mv $path/$name.ind ./
+
+
